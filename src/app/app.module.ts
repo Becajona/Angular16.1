9210fa446a31a6ddd19225a2f96a3813ec9f6882
UTP <jonathanvegagomez15@gmail.com>
2024-02-28 08:47:37 -0600

    Funciones.jv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,17 @@ import { AppComponent } from './app.component';
 import { NavegacionModule } from './navegacion/navegacion.module';
 import { ProductosvideojuegosModule }  from './productosvideojuegos/productosvideojuegos.module';
 import { ProveedoresModule } from './proveedores/proveedores.module';
+import { CatalogoProductosComponent } from './productos.videojuegos/catalogo-productos/catalogo-productos.component';
+import { NuevoProductoComponent } from './productos.videojuegos/nuevo-producto/nuevo-producto.component';
+import { BuscarProductosComponent } from './productos.videojuegos/buscar-productos/buscar-productos.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatalogoProductosComponent,
+    NuevoProductoComponent,
+    BuscarProductosComponent
   ],
   imports: [
     BrowserModule,
